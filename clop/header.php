6381a2c4a2eb857f9399fa8102b7f0de42a06053
Allen Mitchell <elune1@outlook.com>
2023-03-05 19:59:04 +1300

    updating icons / banner

--- clop/header.php
+++ clop/header.php
@@ -1,6 +1,6 @@
 <?php
 include("backend/backend_header.php");
-$headernumber = rand(1, 14);
+$headernumber = rand(1, 15);
 if ($headernumber == 4 || $headernumber == 2) {
     $headerextension = "gif";
 } else {
