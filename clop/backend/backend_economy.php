c79ac704f6beecafef5677a5746391fc2a908297
Allen Mitchell <elune1@outlook.com>
2023-03-07 20:23:59 +1300

    adding economy page

--- clop/backend/backend_economy.php
+++ clop/backend/backend_economy.php
@@ -4,8 +4,5 @@ include_once("allfunctions.php");
 $sql=<<<EOSQL
 SELECT COUNT(*) FROM users WHERE lastactive >= NOW() - INTERVAL 1 DAY
 EOSQL;
-$sth = $GLOBALS['mysqli']->query($sql);
-$rs = onelinequery($sth);
-#$players_active_24h = $rs['COUNT(*)'];
-#$players_active_24h = $sth
+$players_active_24h = onelinequery($sql);
 ?>
