8802225921b6d843aef8e418cf4a05c63410e558
Allen Mitchell <elune1@outlook.com>
2023-03-07 20:28:00 +1300

    adding economy page

--- clop/backend/backend_economy.php
+++ clop/backend/backend_economy.php
@@ -1,8 +1,17 @@
 <?php
 include_once("allfunctions.php");
-#$players_active_24h = "";
 $sql=<<<EOSQL
 SELECT COUNT(*) FROM users WHERE lastactive >= NOW() - INTERVAL 1 DAY
 EOSQL;
 $players_active_24h = onelinequery($sql)['COUNT(*)'];
+
+$sql=<<<EOSQL
+SELECT COUNT(*) FROM users WHERE lastactive >= NOW() - INTERVAL 7 DAY
+EOSQL;
+$players_active_week = onelinequery($sql)['COUNT(*)'];
+
+$sql=<<<EOSQL
+SELECT COUNT(*) FROM users WHERE lastactive >= NOW() - INTERVAL 30 DAY
+EOSQL;
+$players_active_month = onelinequery($sql)['COUNT(*)'];
 ?>
--- clop/economy.php
+++ clop/economy.php
@@ -3,9 +3,11 @@ include("backend/backend_economy.php");
 $extratitle = "Economy - ";
 #include("header.php");
 echo <<<EOFORM
-<center>Players Active Last 24 Hours</center>
+<center>Players Active</center>
 <table class="table table-striped table-bordered">
-<tr><th>Count:</th><th>$players_active_24h</th></tr>
+<tr><th>Last 24 Hours:</th><th>$players_active_24h</th></tr>
+<tr><th>Last 7 Days:</th><th>$players_active_week</th></tr>
+<tr><th>Last 30 Days:</th><th>$players_active_month</th></tr>
 EOFORM;
 echo "</table>";
 #include("footer.php");
