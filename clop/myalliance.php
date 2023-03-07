9a4cb04b495a69a52d04fac57ee3c90d52d39d4c
Allen Mitchell <elune1@outlook.com>
2023-03-07 22:52:32 +1300

    adding nation type & icons to alliance page

--- clop/myalliance.php
+++ clop/myalliance.php
@@ -105,7 +105,7 @@ EOFORM;
         $displaynations = array();
         foreach ($nations[$member['user_id']] as $nation) {
             $displaynations[] =<<<EOFORM
-<a href="viewnation.php?nation_id={$nation['nation_id']}">{$nation['name']} (TEST)</a>
+<a href="viewnation.php?nation_id={$nation['nation_id']}">{$nation['name']} ({$nation['region']})</a>
 EOFORM;
         }
         echo implode(", ", $displaynations);
