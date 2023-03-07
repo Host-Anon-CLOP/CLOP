b8ad76fbfc066bd2bb085879f16671950ab971a8
Allen Mitchell <elune1@outlook.com>
2023-03-07 19:59:20 +1300

    adding economy page

--- clop/economy.php
+++ clop/economy.php
@@ -1,12 +1,12 @@
 <?php
 include("backend/backend_economy.php");
 $extratitle = "Economy - ";
-include("header.php");
+#include("header.php");
 echo <<<EOFORM
 <center>Players Active Last 24 Hours</center>
 <table class="table table-striped table-bordered">
 <tr><th>Count:</th><th>User</th><th>$players_active_24h</th></tr>
 EOFORM;
 echo "</table>";
-include("footer.php");
+#include("footer.php");
 ?>
