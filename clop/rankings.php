52b3e4138e53ec3002c842bc3a0c170dafc4dff7
Allen Mitchell <elune1@outlook.com>
2023-03-11 19:33:02 +1300

    adding resources to myalliance page

--- clop/rankings.php
+++ clop/rankings.php
@@ -44,9 +44,9 @@ EOFORM;
     } else {
         $sortby = $nation['gdp_last_turn'];
     }
-    echo <<<EOFORM
     $regiontypes = array(0 => "The Heavily Fortified Island of Admin", 1 => "Saddle Arabia", 2 => "Zebrica", 3 => "Burrozil", 4 => "Przewalskia");
     $icontypes = array(0 => "Drugs", 1 => "Oil", 2 => "Copper", 3 => "Apples", 4 => "Machinery Parts");
+    echo <<<EOFORM
 <tr><td style="width:25px">{$flaghtml}</td><td><a href="viewnation.php?nation_id={$nation['nation_id']}">{$nation['name']} (<img src="images/icons/{$icontypes[$nation['region']]}.png"/>{$regiontypes[$nation['region']]})</a></td><td>{$sortby}</td><td>{$nation['government']}</td><td>{$nation['economy']}</td></tr>
 EOFORM;
 }
