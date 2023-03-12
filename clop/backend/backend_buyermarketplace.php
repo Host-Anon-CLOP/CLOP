09e02a520f74e31b343aa9fe7004bf440d07c14a
Allen Mitchell <elune1@outlook.com>
2023-03-12 22:13:44 +1300

    adding friends list

--- clop/backend/backend_buyermarketplace.php
+++ clop/backend/backend_buyermarketplace.php
@@ -120,21 +120,6 @@ if ($_POST) {
         $embargoed[$rs['user_id']] = $rs['user_id'];
     }
     }
-
-    $sql = "SELECT u.user_id FROM friends e INNER JOIN users u ON e.friendee = u.user_id WHERE e.friender = '{$_SESSION['user_id']}'";
-    $sth = $GLOBALS['mysqli']->query($sql);
-    if ($sth) {
-    while ($rs = mysqli_fetch_array($sth)) {
-        $friends[$rs['user_id']] = $rs['user_id'];
-    }
-    }
-    $sql = "SELECT u.user_id FROM enemies e INNER JOIN users u ON e.enemiee = u.user_id WHERE e.enemier = '{$_SESSION['user_id']}'";
-    $sth = $GLOBALS['mysqli']->query($sql);
-    if ($sth) {
-    while ($rs = mysqli_fetch_array($sth)) {
-        $enemies[$rs['user_id']] = $rs['user_id'];
-    }
-    }
 }
 if ($_POST['remove']) {
     if ($mysql['sellingto_id'] != $_SESSION['nation_id']) {
@@ -271,6 +256,21 @@ if ($_POST) {
                 $deals[] = $rs;
             }
         }
+    }
+
+    $sql = "SELECT u.user_id FROM friends e INNER JOIN users u ON e.friendee = u.user_id WHERE e.friender = '{$_SESSION['user_id']}'";
+    $sth = $GLOBALS['mysqli']->query($sql);
+    if ($sth) {
+    while ($rs = mysqli_fetch_array($sth)) {
+        $friends[$rs['user_id']] = $rs['user_id'];
+    }
+    }
+    $sql = "SELECT u.user_id FROM enemies e INNER JOIN users u ON e.enemiee = u.user_id WHERE e.enemier = '{$_SESSION['user_id']}'";
+    $sth = $GLOBALS['mysqli']->query($sql);
+    if ($sth) {
+    while ($rs = mysqli_fetch_array($sth)) {
+        $enemies[$rs['user_id']] = $rs['user_id'];
+    }
     }
 }
 }
