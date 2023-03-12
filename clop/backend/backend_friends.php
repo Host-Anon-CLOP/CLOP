ae4bc29b06a7c278e21bd63f3a5fce40d4a548fd
Allen Mitchell <elune1@outlook.com>
2023-03-12 21:41:39 +1300

    adding friends list

--- clop/backend/backend_friends.php
+++ clop/backend/backend_friends.php
@@ -43,31 +43,31 @@ if ($_POST['action'] == "unfriend") {
 }
 
 # ENEMIES
-if ($_POST['action'] == "Friend") {
-    $mysql['Friend'] = $GLOBALS['mysqli']->real_escape_string($_POST['Friend']);
-    $sql = "SELECT user_id FROM users WHERE username = '{$mysql['Friend']}'";
+if ($_POST['action'] == "Enemy") {
+    $mysql['Enemy'] = $GLOBALS['mysqli']->real_escape_string($_POST['Enemy']);
+    $sql = "SELECT user_id FROM users WHERE username = '{$mysql['Enemy']}'";
     $rs = onelinequery($sql);
     if ($rs) {
-        $Friendid = $rs['user_id'];
+        $Enemyid = $rs['user_id'];
     } else {
-        $sql = "SELECT u.user_id FROM users u INNER JOIN nations n ON u.user_id = n.user_id WHERE n.name = '{$mysql['Friend']}'";
+        $sql = "SELECT u.user_id FROM users u INNER JOIN nations n ON u.user_id = n.user_id WHERE n.name = '{$mysql['Enemy']}'";
         $rs = onelinequery($sql);
         if ($rs) {
-            $Friendid = $rs['user_id'];
+            $Enemyid = $rs['user_id'];
         }
     }
-    if ($Friendid == $_SESSION['user_id']) {
+    if ($Enemyid == $_SESSION['user_id']) {
         $errors[] = "Your senior advisors formally request that you put down the crack pipe.";
-    } else if ($Friendid) {
-        $sql = "INSERT INTO friends (friendee, friender) VALUES ({$Friendid}, {$_SESSION['user_id']})";
+    } else if ($Enemyid) {
+        $sql = "INSERT INTO enemies (enemiee, enemier) VALUES ({$Enemyid}, {$_SESSION['user_id']})";
         $GLOBALS['mysqli']->query($sql);
     } else {
         $errors[] = "Name not found.";
     }
 }
-if ($_POST['action'] == "unfriend") {
-    $mysql['unfriend'] = $GLOBALS['mysqli']->real_escape_string($_POST['unfriend']);
-    $sql = "DELETE FROM friends WHERE friendee = '{$mysql['unfriend']}' AND friender = '{$_SESSION['user_id']}'";
+if ($_POST['action'] == "unenemy") {
+    $mysql['unenemy'] = $GLOBALS['mysqli']->real_escape_string($_POST['unenemy']);
+    $sql = "DELETE FROM enemies WHERE enemiee = '{$mysql['unenemy']}' AND enemier = '{$_SESSION['user_id']}'";
     $GLOBALS['mysqli']->query($sql);
 }
 }
