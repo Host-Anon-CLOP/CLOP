8390f74c83dd7e29fd82d16b416bf21e72967645
Allen Mitchell <elune1@outlook.com>
2023-03-14 22:02:48 +1300

    adding resources on test server

--- clop/backend/backend_sendforces.php
+++ clop/backend/backend_sendforces.php
@@ -30,9 +30,9 @@ EOSQL;
 		$sql=<<<EOSQL
 		SELECT COUNT(*) AS count
 		FROM forcegroups 
-		WHERE destination_id = '{$targetnation['nation_id']}'
+		WHERE nation_id = '{$nationinfo['nation_id']}' AND destination_id = '35'
 EOSQL;
-		$attackssent = onelinequery($sql);
+		$attackssent = onelinequery($sql); # destination_id = '{$targetnation['nation_id']}'
         $sql=<<<EOSQL
         SELECT n.*, u.stasismode, u.alliance_id FROM nations n INNER JOIN users u ON n.user_id = u.user_id WHERE n.name = '{$mysql['name']}'
 EOSQL;
