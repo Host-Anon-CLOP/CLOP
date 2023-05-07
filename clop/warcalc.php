f3ca85c1075b37fbdd2a7fc6e4d838384d29ba90
Allen Mitchell <elune1@outlook.com>
2023-05-07 23:57:59 +1200

    updating

--- clop/warcalc.php
+++ clop/warcalc.php
@@ -3,13 +3,13 @@ $extratitle = "War Calc - ";
 echo <<<EOFORM
 <center><h4>Create New Force</h4></center>
 <center><form action="warcalc.php" method="post">
+<input name="name" class="form-control" placeholder="Name" style="width:50px;"/>
 <select name="forcetype" class="form-control" style="width:200px;"/>
     <option value="1">1</option>
     <option value="2">2</option>
 </select>
-<input name="size" class="form-control" placeholder="Size" style="width:50px;"/><br/>
-<input name="name" class="form-control" placeholder="Name" style="width:50px;"/><br/>
-<input name="training" class="form-control" placeholder="Training" style="width:50px;"/><br/>
+<input name="size" class="form-control" placeholder="Size" style="width:50px;"/>
+<input name="training" class="form-control" placeholder="Training" style="width:50px;"/>
 <input type="submit" name="createforce" value="Create Force" class="btn btn-info"/>
 </form>
 EOFORM;
