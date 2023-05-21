f24a29a1513d09f8f3513e3b7bb80799aced307f
Allen Mitchell <elune1@outlook.com>
2023-05-21 22:36:28 +1200

    test

--- clop/backend/backend_test.php
+++ clop/backend/backend_test.php
@@ -17,7 +17,7 @@ var element = document.getElementById('group-fields');
 var children = element.children;
 for(var i=0; i<children.length; i++){
     var child = children[i];
-    echo "{$child.text}";
+    echo "{$child.outerHTML}";
   }
 
 ?>
