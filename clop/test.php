33b9c32f1bf93e69f834f3407f271a0cb2672efa
Allen Mitchell <elune1@outlook.com>
2023-05-21 16:04:49 +1200

    test

--- clop/test.php
+++ clop/test.php
@@ -12,7 +12,23 @@
           '<option value="unicorns">Unicorns</option>';
         groupDiv.appendChild(unitSelect);
 
-                
+
+                } else if (unittype == 'Pegasi') {
+          armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+          '<option value="cooler">Cooler</option>' +
+          '<option value="wonder">Wonder</option>' +
+          '<option value="griffin">Griffin</option>' +
+          '<option value="dragon">Dragon</option>';
+        }
+
+
+                      
+        } else if (unittype == 'Navy') {
+          armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+          '<option value="c-pon3">C-PON3</option>' +
+          '<option value="esohes">Esohes</option>' +
+          '<option value="shubidu">Shubidu</option>';
+        }  
 */
 
 echo <<<EOFORM
@@ -66,13 +82,13 @@ echo <<<EOFORM
           '<option value="ohno">Ohno</option>' +
           '<option value="titan">Titan</option>';
         }
-        } else if (unittype == 'Pegasi') {
-          armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
-          '<option value="cooler">Cooler</option>' +
-          '<option value="wonder">Wonder</option>' +
-          '<option value="griffin">Griffin</option>' +
-          '<option value="dragon">Dragon</option>';
-        }
+      } else if (unittype == 'Unicorns') {
+        armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="hornshield">Hornshield</option>' +
+        '<option value="librarian">Librarian</option>' +
+        '<option value="shining">Shining</option>' +
+        '<option value="d2a">D2A</option>';
+      }
 
         groupDiv.appendChild(armorSelect);
 
