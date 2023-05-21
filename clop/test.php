d9fe4e024f3cfbb8152e6638ac5374e42d2ea05b
Allen Mitchell <elune1@outlook.com>
2023-05-21 22:46:29 +1200

    test

--- clop/test.php
+++ clop/test.php
@@ -33,8 +33,17 @@ echo <<<EOFORM
     <button type="button" onclick="addNewGroup()">Add Group</button>
 
     <script>
- 
+    function calculate() {
+      var element = document.getElementById('group-fields');
+      var children = element.children;
+      for(var i=0; i<children.length; i++){
+          var child = children[i];
+          echo "$child.outerHTML";
+        }
+      }
+    </script>
 
+    <script>
       function addNewGroup() {
         var e = document.getElementById("unit-type");
         var value = e.options[e.selectedIndex].value;
