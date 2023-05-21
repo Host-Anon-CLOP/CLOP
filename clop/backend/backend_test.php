b9bc541fb4e0013a6af9846c13e60503e81e8d7f
Allen Mitchell <elune1@outlook.com>
2023-05-21 22:39:02 +1200

    test

--- clop/backend/backend_test.php
+++ clop/backend/backend_test.php
@@ -7,7 +7,7 @@ var element = document.getElementById('group-fields');
 var children = element.children;
 for(var i=0; i<children.length; i++){
     var child = children[i];
-    echo "{$child.outerHTML}";
+    echo "$child.outerHTML";
   }
 </script>
 EOFORM;
