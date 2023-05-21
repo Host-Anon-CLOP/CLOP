2720badcb28aafce482959a09b4136f3a54eaf6d
Allen Mitchell <elune1@outlook.com>
2023-05-21 23:28:54 +1200

    test

--- clop/backend.php
+++ clop/backend.php
@@ -1,49 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Form Submission Results</title>
-</head>
-<body>
-  <h1>Form Submission Results</h1>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $attackerData = [];
+    $defenderData = [];
 
-  <table>
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Weapon</th>
-        <th>Armor</th>
-        <th>Size</th>
-        <th>Training</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $types = $_POST['type'];
-        $weapons = $_POST['weapon'];
-        $armors = $_POST['armor'];
-        $sizes = $_POST['size'];
-        $trainings = $_POST['training'];
-
-        // Loop through the submitted values and display them
-        for ($i = 0; $i < count($types); $i++) {
-          $type = $types[$i];
-          $weapon = $weapons[$i];
-          $armor = $armors[$i];
-          $size = $sizes[$i];
-          $training = $trainings[$i];
-
-          echo "<tr>";
-          echo "<td>$type</td>";
-          echo "<td>$weapon</td>";
-          echo "<td>$armor</td>";
-          echo "<td>$size</td>";
-          echo "<td>$training</td>";
-          echo "</tr>";
+    // Retrieve data for attackers
+    if (!empty($_POST['type']) && is_array($_POST['type'])) {
+        foreach ($_POST['type'] as $index => $type) {
+            if (!empty($type)) {
+                $attackerData[] = [
+                    'type' => $type,
+                    'weapon' => $_POST['weapon'][$index],
+                    'armor' => $_POST['armor'][$index],
+                    'size' => $_POST['size'][$index],
+                    'training' => $_POST['training'][$index]
+                ];
+            }
         }
-      }
-      ?>
-    </tbody>
-  </table>
-</body>
-</html>
+    }
+
+    // Retrieve data for defenders
+    // ...
+
+    // Display the entered data
+    echo "<h2>Attacker Data:</h2>";
+    echo "<pre>";
+    print_r($attackerData);
+    echo "</pre>";
+
+    echo "<h2>Defender Data:</h2>";
+    // Display defender data
+    // ...
+}
+?>
