846fa4fccaad94de29e168eb91c223beb77224b0
Allen Mitchell <elune1@outlook.com>
2023-05-21 22:55:13 +1200

    test

--- clop/test.php
+++ clop/test.php
@@ -13,6 +13,19 @@
 
 */
 
+/*
+    <script>
+      function calculate() {
+        var element = document.getElementById('group-fields');
+        var children = element.children;
+        for(var i=0; i<children.length; i++){
+            var child = ;
+            <div class="panel-heading h4">{$child.outerHTML}</div>
+          }
+        }
+    </script>
+    */
+
 echo <<<EOFORM
 <html>
 <body>
@@ -33,15 +46,97 @@ echo <<<EOFORM
     <button type="button" onclick="addNewGroup()">Add Group</button>
 
     <script>
-      function calculate() {
-        var element = document.getElementById('group-fields');
-        var children = element.children;
-        for(var i=0; i<children.length; i++){
-            var child = children[i];
-            <div class="panel-heading h4">{$child.outerHTML}</div>
-          }
-        }
-    </script>
+    function calculate() {
+      var e = document.getElementById("unit-type");
+      var value = e.options[e.selectedIndex].value;
+      var unittype = e.options[e.selectedIndex].text;
+
+      const groupFields = document.getElementById('group-fields');
+
+      const groupDiv = document.createElement('div');
+
+      const unitLabel = document.createElement('label');
+      unitLabel.textContent = 'Unit: ' + unittype;
+      groupDiv.appendChild(unitLabel);
+
+      const weaponLabel = document.createElement('label');
+      weaponLabel.textContent = 'Weapon:';
+      groupDiv.appendChild(weaponLabel);
+
+      const weaponSelect = document.createElement('select');
+      weaponSelect.name = 'weapon';
+      if (unittype == 'Cavalry') {
+      weaponSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="prc-e6">PRC-E6</option>' +
+        '<option value="prc-e7">PRC-E7</option>' +
+        '<option value="prc-e8">PRC-E8</option>' +
+        '<option value="acfu">ACFU</option>' +
+        '<option value="apfu">APFU</option>' +
+        '<option value="atfu">ATFU</option>' +
+        '<option value="aufu">AUFU</option>';
+      } if (unittype == 'Tanks') {
+      weaponSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="k9p">K9P</option>' +
+        '<option value="elbo-grs">ELBO-GRS</option>' +
+        '<option value="chem-light">Chem-Light</option>';
+      } else if (unittype == 'Pegasi') {
+      weaponSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="propwash">PropWash</option>' +
+        '<option value="steambucket">SteamBucket</option>' +
+        '<option value="canopylights">CanopyLights</option>';
+      } else if (unittype == 'Unicorns') {
+      weaponSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="longstand">LongStand</option>' +
+        '<option value="longweight">LongWeight</option>' +
+        '<option value="gridsquares">GridSquares</option>';
+      } else if (unittype == 'Navy') {
+      weaponSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="shoreline">Shoreline</option>' +
+        '<option value="waterhammer">WaterHammer</option>' +
+        '<option value="waterlineeraser">WaterlineEraser</option>';
+      }
+      groupDiv.appendChild(weaponSelect);
+
+      const armorLabel = document.createElement('label');
+      armorLabel.textContent = ' Armor:';
+      groupDiv.appendChild(armorLabel);
+
+      const armorSelect = document.createElement('select');
+      armorSelect.name = 'armor';
+      if (unittype == 'Cavalry') {
+      armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="barding">Barding</option>' +
+        '<option value="bigdog">Bigdog</option>' +
+        '<option value="nope">Nope</option>';
+      } else if (unittype == 'Tanks') {
+        armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="trundle">Trundle</option>' +
+        '<option value="shepherd">Shepherd</option>' +
+        '<option value="ohno">Ohno</option>' +
+        '<option value="titan">Titan</option>';
+      } else if (unittype == 'Pegasi') {
+        armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="cooler">Cooler</option>' +
+        '<option value="wonder">Wonder</option>' +
+        '<option value="griffin">Griffin</option>' +
+        '<option value="dragon">Dragon</option>';
+      } else if (unittype == 'Unicorns') {
+        armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="hornshield">Hornshield</option>' +
+        '<option value="librarian">Librarian</option>' +
+        '<option value="shining">Shining</option>' +
+        '<option value="d2a">D2A</option>';
+      } else if (unittype == 'Navy') {
+        armorSelect.innerHTML = '<option value="scrounged">Scrounged</option>' +
+        '<option value="c-pon3">C-PON3</option>' +
+        '<option value="esohes">Esohes</option>' +
+        '<option value="shubidu">Shubidu</option>';
+      }
+      groupDiv.appendChild(armorSelect);
+
+      groupFields.appendChild(groupDiv);
+    }
+  </script>
 
     <script>
       function addNewGroup() {
