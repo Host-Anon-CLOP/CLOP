aac2d6a62d36d72360439054ca52f74313b98a94
Allen Mitchell <elune1@outlook.com>
2023-05-21 22:37:25 +1200

    test

--- clop/backend/backend_test.php
+++ clop/backend/backend_test.php
@@ -3,21 +3,15 @@ include_once("allfunctions.php");
 
 echo <<<EOFORM
 <script>
-const groupFieldsBackend = document.getElementById('')
-</script>
-
-foreach ($group in $groupFieldsBackend) {
-	
-<tr><td>$group</td></tr>
-
-	}
-EOFORM;
-
 var element = document.getElementById('group-fields');
 var children = element.children;
 for(var i=0; i<children.length; i++){
     var child = children[i];
     echo "{$child.outerHTML}";
   }
+</script>
+EOFORM;
+
+
 
 ?>
