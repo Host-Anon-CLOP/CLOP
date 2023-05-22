fc51c16aa0c4d0cf4bfd3cb05270173a688c6bc7
Allen Mitchell <elune1@outlook.com>
2023-05-23 02:00:18 +1200

    updating

--- clop/backend.php
+++ clop/backend.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         foreach ($_POST['type'] as $index => $type) {
             if (!empty($type)) {
                 #$name = $_POST['weapon'][$index] + '_' + $_POST['armor'][$index] + '_' + $_POST['training'][$index] + '_' + $_POST['size'][$index];
-                $name = $_POST['weapon'][$index] + '_';
+                $name = $_POST['weapon'][$index] . '_';
 
                 $attackerData[] = [
                     'type' => $type,
