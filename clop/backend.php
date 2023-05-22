803cce58b081267f271d569d914c61dec658f07e
Allen Mitchell <elune1@outlook.com>
2023-05-23 00:31:45 +1200

    updating

--- clop/backend.php
+++ clop/backend.php
@@ -3,8 +3,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $attackerData = [];
     $defenderData = [];
 
-    $forcetypes = array("Cavalry" => 1, 2 => "Tanks", 3 => "Pegasi", 4 => "Unicorns", 5 => "Naval", 6 => "Alicorns");
-
+    $forcetypes = array("Cavalry" => 1, "Tanks" => 2, "Pegasi" => 3, "Unicorns" => 4, "Naval" => 5, "Alicorns" => 6);
+    $weapontypes = array("Scrounged" => 0, "PRC-E6" => 1,"PRC-E7" => 2,"PRC-E8" => 3,"ACFU" => 4,"ATFU" => 5,"APFU" => 6,"AUFU" => 7,"K9P" => 8,"ELBO-GRS" => 9,"Chem-Light" => 10,"PropWash" => 11,"SteamBucket" => 12,"CanopyLights" => 13,"LongStand" => 14,"LongWeight" => 15,"GridSquares" => 16,"Shoreline" => 17,"WaterHammer" => 18,"WaterlineEraser" => 19);
+    $armortypes = array("Scrounged" => 0,"Barding" => 1,"Bigdog" => 2,"Nope" => 3,"Trundle" => 4,"Shepherd" => 5,"Ohno" => 6,"Titan" => 7,"Cooler" => 8,"Wonder" => 9,"Griffin" => 10,"Dragon" => 11,"Hornshield" => 12,"Librarian" => 13,"Shining" => 14,"D2A" => 15,"C-PON3" => 16,"Esohes" => 17,"Shubidu" => 18);
+    
     // Retrieve data for attackers
     if (!empty($_POST['type']) && is_array($_POST['type'])) {
         foreach ($_POST['type'] as $index => $type) {
@@ -13,7 +15,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     'type' => $type,
                     'forcetype' => $forcetypes[$type],
                     'weapon' => $_POST['weapon'][$index],
+                    'weapontypes' => $weapontypes[$_POST['weapon'][$index]],
                     'armor' => $_POST['armor'][$index],
+                    'armortypes' => $armortypes[$_POST['armor'][$index]],
                     'size' => $_POST['size'][$index],
                     'training' => $_POST['training'][$index]
                 ];
