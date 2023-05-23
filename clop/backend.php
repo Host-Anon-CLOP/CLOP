c01e74097bd9431d013c3e6226c660fac2722cd5
Allen Mitchell <elune1@outlook.com>
2023-05-23 14:28:05 +1200

    updating

--- clop/backend.php
+++ clop/backend.php
@@ -9,9 +9,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $weapontypes = array("Scrounged" => 0, "PRC-E6" => 1,"PRC-E7" => 2,"PRC-E8" => 3,"ACFU" => 4,"ATFU" => 5,"APFU" => 6,"AUFU" => 7,"K9P" => 8,"ELBO-GRS" => 9,"Chem-Light" => 10,"PropWash" => 11,"SteamBucket" => 12,"CanopyLights" => 13,"LongStand" => 14,"LongWeight" => 15,"GridSquares" => 16,"Shoreline" => 17,"WaterHammer" => 18,"WaterlineEraser" => 19);
     $armortypes = array("Scrounged" => 0,"Barding" => 1,"Bigdog" => 2,"Nope" => 3,"Trundle" => 4,"Shepherd" => 5,"Ohno" => 6,"Titan" => 7,"Cooler" => 8,"Wonder" => 9,"Griffin" => 10,"Dragon" => 11,"Hornshield" => 12,"Librarian" => 13,"Shining" => 14,"D2A" => 15,"C-PON3" => 16,"Esohes" => 17,"Shubidu" => 18);
 
-    // Retrieve data for attackers
-    if (!empty($_POST['attack_type']) && is_array($_POST['attack_type'])) {
-
 # Clear Previous Results
 $sql=<<<EOSQL
 truncate forcegroups_calc
@@ -36,12 +33,15 @@ EOSQL;
 $GLOBALS['mysqli']->query($sql);
 
 
+    // Retrieve data for attackers
+    if (!empty($_POST['attack_type']) && is_array($_POST['attack_type'])) {
+
         foreach ($_POST['attack_type'] as $index => $which_type) {
             if (!empty($which_type)) {
                 $name = $which_type . '_' . $_POST['weapon'][$index] . '_' . $_POST['armor'][$index] . '_size_' . $_POST['size'][$index]. '_train_' . $_POST['training'][$index];
 
                 $attackerData[] = [
-                    'unit' => $forcetypes[$which_type],
+                    'unit' => $which_type,
                     'weapon' => $_POST['weapon'][$index],
                     'armor' => $_POST['armor'][$index],
                     'size' => $_POST['size'][$index],
