34aad1eed4ecf4bbf908d4eb1864962acf11ade0
Allen Mitchell <elune1@outlook.com>
2023-06-02 21:48:25 +1200

    updating

--- clop/ascend-calc.php
+++ clop/ascend-calc.php
@@ -6,7 +6,7 @@ echo <<<EOFORM
 <center><form action="ascend-calc.php" method="post">
 <input name="size" class="form-control" placeholder="Size" style="width:200px;"/>
 <input name="name" class="form-control" placeholder="Name" style="width:200px;"/><br/>
-<input type="submit" name="createforce" value="Create Force" class="btn btn-info"/>
+<input type="submit" name="ascend-calc" value="Create Force" class="btn btn-info"/>
 </form>
 EOFORM;
 ?>
