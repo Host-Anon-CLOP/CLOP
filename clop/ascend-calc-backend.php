b453a299c96b7d55175a1d2206468bd9a479c761
Allen Mitchell <elune1@outlook.com>
2023-06-02 22:09:19 +1200

    updating

--- clop/ascend-calc-backend.php
+++ clop/ascend-calc-backend.php
@@ -13,8 +13,8 @@ if ($_POST['ascend-calc']) {
         $current_rep += 1;
         $new_pegasi = ceil($current_rep / 4);
         $total_pegasi = $total_pegasi + $new_pegasi;
-        echo "tick " . $x . "<br>";
         echo "<br>";
+        echo "tick " . $x . "<br>";
         echo "rep: " . $current_rep . "<br>";
         echo "new_pegasi: " . $new_pegasi . "<br>";
         echo "total pegasi: " . $total_pegasi . "<br>";
