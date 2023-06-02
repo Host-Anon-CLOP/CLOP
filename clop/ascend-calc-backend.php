d38aeae8aac62e35a4fe8be9ae838fc699aba29f
Allen Mitchell <elune1@outlook.com>
2023-06-02 21:39:32 +1200

    updating

--- clop/ascend-calc-backend.php
+++ clop/ascend-calc-backend.php
@@ -1,13 +1,6 @@
 <?php
-require_once("backend/allfunctions.php");
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $superpower_count = $_POST['weapon'][$count]
-
-    echo "<h2>Attacker Data:</h2>";
-    echo "<pre>";
-    foreach ($attackerData as $attacker) {
-        echo $attacker['unit'] . ' ' . $attacker['weapon'] . ' ' . $attacker['armor'] . ' size:' . $attacker['size'] . ' train:' . $attacker['training'] . '<br>';
-    }
-    echo "</pre>";
+include_once("allfunctions.php");
+if ($_POST['ascend-calc']) {
+    echo (int)$_POST['size'];
 }
 ?>
--- clop/ascend-calc.php
+++ clop/ascend-calc.php
@@ -1,9 +1,9 @@
 <?php
 include("ascend-calc-backend.php");
-$extratitle = "Create Forces - ";
+$extratitle = "Ascend Calc - ";
 
 echo <<<EOFORM
-</select><br/>
+<center><form action="ascend-calc.php" method="post">
 <input name="size" class="form-control" placeholder="Size" style="width:200px;"/>
 <input name="name" class="form-control" placeholder="Name" style="width:200px;"/><br/>
 <input type="submit" name="createforce" value="Create Force" class="btn btn-info"/>
