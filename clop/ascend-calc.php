d6ce813214478914785e12ff5c6bc204475289d1
Allen Mitchell <elune1@outlook.com>
2023-06-02 21:31:03 +1200

    updating

--- clop/ascend-calc.php
+++ clop/ascend-calc.php
@@ -4,9 +4,10 @@ include("ascend-calc-backend.php");
 $extratitle = "Create Forces - ";
 
 echo <<<EOFORM
-<center><form action="createforces.php" method="post">
-<input type="hidden" name="token_createforces" value="{$_SESSION['token_createforces']}"/>
-<select name="forcetype" class="form-control" style="width:210px;"/>
-<option value=""></option>
+</select><br/>
+<input name="size" class="form-control" placeholder="Size" style="width:200px;"/>
+<input name="name" class="form-control" placeholder="Name" style="width:200px;"/><br/>
+<input type="submit" name="createforce" value="Create Force" class="btn btn-info"/>
+</form>
 EOFORM;
 ?>
