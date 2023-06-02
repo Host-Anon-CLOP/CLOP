2a69759305b861048b163e712dbad66dc7d582f9
Allen Mitchell <elune1@outlook.com>
2023-06-02 21:59:49 +1200

    updating

--- clop/ascend-calc-backend.php
+++ clop/ascend-calc-backend.php
@@ -5,6 +5,10 @@ if ($_POST['ascend-calc']) {
     echo "ticks " . $_POST['ticks'] . "<br>";
     echo "rep " . $_POST['reputation'] . "<br>";
     echo "pegasi " . $_POST['pegasi'] . "<br>";
+
+    for ($x = 0; $x <= $_POST['ticks']; $x++) {
+        echo "The number is: $x <br>";
+      }
     
 }
 ?>
