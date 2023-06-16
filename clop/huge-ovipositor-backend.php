10d1df563f2e34671ad892af4fbe3049d2ffd93f
Allen Mitchell <elune1@outlook.com>
2023-06-17 02:12:36 +1200

    updating

--- clop/huge-ovipositor-backend.php
+++ clop/huge-ovipositor-backend.php
@@ -345,15 +345,16 @@ while ($rs = mysqli_fetch_array($sth)) {
 
 echo "<br><br>";
 echo "<h2>Remaining Defenders:</h2><br>";
-$sql = "SELECT fc.size, fc.type, fc.weapon_id, fc.armor_id, fc.training, fc.name, fc.forcegroup_id, wd.dmg_cavalry, wd.dmg_tanks, wd.dmg_pegasi, wd.dmg_unicorns, wd.dmg_naval FROM forces_calc fc INNER JOIN weapondefs wd ON fc.weapon_id = wd.weapon_id WHERE forcegroup_id = '2'";
+$sql = "SELECT fc.size, fc.type, fc.weapon_id, fc.armor_id, fc.training, fc.name, fc.forcegroup_id, wd.dmg_cavalry, wd.dmg_tanks, wd.dmg_pegasi, wd.dmg_unicorns, wd.dmg_naval, ad.arm_cavalry, ad.arm_tanks, ad.arm_pegasi, ad.arm_unicorns, ad.arm_naval FROM forces_calc fc INNER JOIN weapondefs wd ON fc.weapon_id = wd.weapon_id INNER JOIN armordefs ad ON fc.armor_id = ad.armor_id WHERE forcegroup_id = '2'";
 $sth = $GLOBALS['mysqli']->query($sql);
 while ($rs = mysqli_fetch_array($sth)) {
 	echo $rs['name'] . " LEFT: " . $rs['size'] . "<br>";
-	echo "dmg peg: " . $rs['dmg_pegasi'] . "<br>";
-	echo "dmg tnk: " . $rs['dmg_tanks'] . "<br>";
-	echo "dmg uni: " . $rs['dmg_unicorns'] . "<br>";
-	echo "dmg cav: " . $rs['dmg_cavalry'] . "<br>";
-	echo "dmg nav: " . $rs['dmg_naval'] . "<br>";
+	echo "dmg peg: " . $rs['dmg_pegasi'] . " arm peg: " . $rs['arm_pegasi'] . "<br>";
+	echo "dmg tnk: " . $rs['dmg_tanks'] . " arm tnk: " . $rs['arm_tanks'] ."<br>";
+	echo "dmg uni: " . $rs['dmg_unicorns'] . " arm uni: " . $rs['arm_unicorns'] ."<br>";
+	echo "dmg cav: " . $rs['dmg_cavalry'] . "arm cav: " . $rs['arm_cavalry'] . "<br>";
+	echo "dmg nav: " . $rs['dmg_naval'] . "arm nav: " . $rs['arm_naval'] ."<br>";
+	echo "<br>"
 }
 
 echo "<br><br>";
