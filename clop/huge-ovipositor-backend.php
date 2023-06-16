b2d86a9722f15b3e99c4151adbf3e8942bb2105e
Allen Mitchell <elune1@outlook.com>
2023-06-17 01:59:20 +1200

    updating

--- clop/huge-ovipositor-backend.php
+++ clop/huge-ovipositor-backend.php
@@ -345,15 +345,15 @@ while ($rs = mysqli_fetch_array($sth)) {
 
 echo "<br><br>";
 echo "<h2>Remaining Defenders:</h2><br>";
-$sql = "SELECT nation_id, size, type, weapon_id, armor_id, training, name, forcegroup_id, dmg_cavalry, dmg_tanks, dmg_pegasi, dmg_unicorns, dmg_naval FROM forces_calc fc INNER JOIN weapondefs wd ON fc.weapon_id = wd.weapon_id WHERE forcegroup_id = 2";
+$sql = "SELECT nation_id, size, type, weapon_id, armor_id, training, name, forcegroup_id FROM forces_calc fc INNER JOIN weapondefs wd ON fc.weapon_id = wd.weapon_id WHERE forcegroup_id = 2";
 $sth = $GLOBALS['mysqli']->query($sql);
 while ($rs = mysqli_fetch_array($sth)) {
 	echo $rs['name'] . " LEFT: " . $rs['size'] . "<br>";
-	echo "dmg peg: " . $rs['dmg_pegasi'] . "<br>";
-	echo "dmg tnk: " . $rs['dmg_tanks'] . "<br>";
-	echo "dmg uni: " . $rs['dmg_unicorns'] . "<br>";
-	echo "dmg cav: " . $rs['dmg_cavalry'] . "<br>";
-	echo "dmg nav: " . $rs['dmg_naval'] . "<br>";
+	#echo "dmg peg: " . $rs['dmg_pegasi'] . "<br>";
+	##echo "dmg tnk: " . $rs['dmg_tanks'] . "<br>";
+	#echo "dmg uni: " . $rs['dmg_unicorns'] . "<br>";
+	#echo "dmg cav: " . $rs['dmg_cavalry'] . "<br>";
+	#echo "dmg nav: " . $rs['dmg_naval'] . "<br>";
 }
 
 echo "<br><br>";
