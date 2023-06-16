85a05693db6ebc613c0bc129620eeca86cd34d9e
Allen Mitchell <elune1@outlook.com>
2023-06-17 01:42:20 +1200

    updating

--- clop/huge-ovipositor-backend.php
+++ clop/huge-ovipositor-backend.php
@@ -337,13 +337,18 @@ EOSQL;
 
 echo "<br><br>";
 echo "Remaining Attackers: (not coded)";
+$sql = "SELECT nation_id, size, type, weapon_id, armor_id, training, name, forcegroup_id FROM forces_calc WHERE forcegroup_id = 1";
+$sth = $GLOBALS['mysqli']->query($sql);
+while ($rs = mysqli_fetch_array($sth)) {
+	echo $rs['name'] . " remaining: " . $rs['size'];
+}
 
 echo "<br><br>";
 echo "Remaining Defenders: (not coded)";
 $sql = "SELECT nation_id, size, type, weapon_id, armor_id, training, name, forcegroup_id FROM forces_calc WHERE forcegroup_id = 2";
 $sth = $GLOBALS['mysqli']->query($sql);
 while ($rs = mysqli_fetch_array($sth)) {
-	echo "result: " . $rs['name'];
+	echo $rs['name'] . " remaining: " . $rs['size'];
 }
 
 
