e03ddbdd163032e82da67caa886919ba1c3dc301
Allen Mitchell <elune1@outlook.com>
2023-06-19 02:33:26 +1200

    updating

--- clop/huge-ovipositor-backend.php
+++ clop/huge-ovipositor-backend.php
@@ -144,7 +144,7 @@ $sth = $GLOBALS['mysqli']->query($sql);
 while ($rs = mysqli_fetch_array($sth)) {
 	$Initial_Defenders = $Initial_Defenders + $rs['size'];
 	$Defenders_Initial_{array_search($rs['type'], $forcetypes)} = $Defenders_Initial_{array_search($rs['type'], $forcetypes)} + $rs['size'];
-	$Defenders_Damage_Initial_{$key} = $Defenders_Damage_Initial_{$key} + $rs["dmg_" . array_search($rs['type'], $forcetypes)"];
+	$Defenders_Damage_Initial_{$key} = $Defenders_Damage_Initial_{$key} + $rs["dmg_" . array_search($rs['type'], $forcetypes)];
 	echo "test: " . $Defenders_Damage_Initial_{$key};
 }
 
