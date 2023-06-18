5fbf8c4929d8fc2024caeec926e1e767b48bffaa
Allen Mitchell <elune1@outlook.com>
2023-06-18 22:16:06 +1200

    updating

--- clop/huge-ovipositor-backend.php
+++ clop/huge-ovipositor-backend.php
@@ -128,10 +128,11 @@ $GLOBALS['mysqli']->query($sql);
 $sql = "SELECT * from forces_calc fc WHERE forcegroup_id = '1' ORDER BY size DESC";
 $sth = $GLOBALS['mysqli']->query($sql);
 while ($rs = mysqli_fetch_array($sth)) {
-	echo $rs['name'] . "size:" . $rs['size'] . " type:" . array_search($rs['type'], $forcetypes) . "<br>";
-	#$Initial_Attackers = 0;
-	#$Attackers_Initial_{$key} = 0;
-}
+	$Initial_Attackers = $Initial_Attackers + $rs['size'];
+	$Attackers_Initial_{array_search($rs['type'], $forcetypes)} = $Attackers_Initial_{array_search($rs['type'], $forcetypes) + $rs['size'];
+	echo "<br>amount: Attackers_Initial_" . array_search($rs['type'], $forcetypes) . " size: " . $Attackers_Initial_{array_search($rs['type'], $forcetypes);
+	}
+echo "attackers total: " . $Initial_Attackers;
 # Defenders
 
     echo "<h2>Battle Result</h2>";
