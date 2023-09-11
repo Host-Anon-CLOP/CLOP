dad9771aa3a0dce411f3940cf45dd8126ac5c338
Allen Mitchell <elune1@outlook.com>
2023-09-12 10:56:40 +1200

    updating

--- clop/backend/backend_topmessage.php
+++ clop/backend/backend_topmessage.php
@@ -1,3 +1,17 @@
 <?php
 include_once("allfunctions.php");
+
+$sql=<<<EOSQL
+SELECT time, message FROM topmessage ORDER BY time DESC
+EOSQL;
+$sth = $GLOBALS['mysqli']->query($sql);
+
+while ($rs = mysqli_fetch_array($sth)) {
+	$message = htmlentities($rs['message'], ENT_SUBSTITUTE, "UTF-8");
+	echo <<<EOFORM
+<tr><td>$message</td></tr>
+EOFORM;
+}
+
+
 ?>
--- clop/topmessage.php
+++ clop/topmessage.php
@@ -1,29 +1,17 @@
 <?php
-include("backend/backend_topmessage.php");
+
 $extratitle = "Top Message - ";
 include("header.php");
-
-$sql=<<<EOSQL
-SELECT time, message FROM topmessage ORDER BY time DESC
-EOSQL;
-$sth = $GLOBALS['mysqli']->query($sql);
 
 echo <<<EOFORM
 <table>
 EOFORM;
 
-while ($rs = mysqli_fetch_array($sth)) {
-	$message = htmlentities($rs['message'], ENT_SUBSTITUTE, "UTF-8");
-	echo <<<EOFORM
-<tr><td>$message</td></tr>
-EOFORM;
-}
+include("backend/backend_topmessage.php");
 
 echo <<<EOFORM
 </table>
 EOFORM;
-
-
 
 include("footer.php");
 ?>
