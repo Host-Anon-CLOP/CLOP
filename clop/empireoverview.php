42df52fee27d48e05f282c164233cba2ff9c7358
Allen Mitchell <elune1@outlook.com>
2023-09-12 23:44:18 +1200

    updating

--- clop/empireoverview.php
+++ clop/empireoverview.php
@@ -7,7 +7,7 @@ echo <<<EOFORM
 <div class="row">
   <div class="col-md-6">
    <div class="panel panel-default">
-     <div class="panel-heading">Resources - stockpiles / net / ticksworth</div>
+     <div class="panel-heading">Resources - Stockpiles</div>
      <table class="table">
       <thead>
         <tr>
@@ -15,7 +15,7 @@ echo <<<EOFORM
 EOFORM;
         if (!$nationinfo['hideicons']) {
             echo <<<EOFORM
-              <td></td>
+            <td style="width: 16px;"></td>
 EOFORM;
         }
         echo <<<EOFORM
@@ -62,75 +62,8 @@ echo <<<EOFORM
 EOFORM;
 }
 
-/*
-# Global Resources
-echo <<<EOFORM
-<div class="row">
-  <div class="col-md-6">
-   <div class="panel panel-default">
-     <div class="panel-heading">Global Resources</div>
-     <table class="table">
-      <thead>
-        <tr>
-EOFORM;
-        if (!$nationinfo['hideicons']) {
-        echo <<<EOFORM
-          <td></td>
-EOFORM;
-        }
-        echo <<<EOFORM
-          <td style="text-align: right;">Resource</td>
-          <td>Generated</td>
-          <td>Used</td>
-          <td>Net</td>
-        </tr>
-      </thead>
-      <tbody>
-EOFORM;
-foreach($affectedresources as $name => $amount) {
-if (!$resources[$name]) $resources[$name] = 0;
-}
-foreach($requiredresources as $name => $amount) {
-if (!$resources[$name]) $resources[$name] = 0;
-}
-ksort($resources);
-foreach($resources as $name => $amount) {
-  if (!$affectedresources[$name]) {
-    $affectedresources[$name] = 0;
-  }
-  if (!$requiredresources[$name]) {
-    $requiredresources[$name] = 0;
-  }
-  $amountNet = ($affectedresources[$name] - $requiredresources[$name]);
 
-  if($amountNet > 0) $amountNetClass = "text-success";
-  elseif($amountNet == 0) $amountNetClass = "text-warning";
-  else $amountNetClass = "text-danger";
-  $displayaffected = commas($affectedresources[$name]);
-  $displayrequired = commas($requiredresources[$name]);
-  if ($amountNet < 0) {
-  $displaynet = "-" . commas(abs($amountNet));
-  } else {
-  $displaynet = commas($amountNet);
-  }
 
-  echo <<<EOFORM
-    <tr>
-EOFORM;
-    if (!$nationinfo['hideicons']) {
-    echo <<<EOFORM
-    <td style="width: 16px;"><img src="images/icons/{$name}.png"/></td>
-EOFORM;
-    }
-    echo <<<EOFORM
-    <td style="text-align: right;">{$name}</td>
-    <td style="text-align: center;"><span class="text-success">{$displayaffected}</span></td>
-    <td style="text-align: center;"><span class="text-danger">{$displayrequired}</span></td>
-    <td style="text-align: center;"><span class="{$amountNetClass}">{$displaynet}</span></td>
-    </tr>
-EOFORM;
-}
-*/
 
 include("footer.php");
 ?>
