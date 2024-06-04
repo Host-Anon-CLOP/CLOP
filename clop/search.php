debcdfbd6f6eda937ba5e0c11ac273e18578e38d
Allen Mitchell <elune1@outlook.com>
2024-06-04 21:23:56 +1200

    adding additional search options

--- clop/search.php
+++ clop/search.php
@@ -4,11 +4,14 @@ $extratitle = "Search - ";
 include("header.php");
 echo <<<EOFORM
 <center><form action="search.php" method="post">
+Search individual User or Nation:
 <input name="name" class="form-control" placeholder="User or Nation Name" style="width:300px"/>
 <input type="submit" name="search" value="Search" class="btn btn-success"/>
 <br>
 <br>
+<br>
 
+Search by Category:
 <input type="submit" name="allianceless" value="Unallied Players" class="btn btn-success btn-sm"/>
 <input type="submit" name="prze" value="Prze" class="btn btn-success btn-sm"/>
 <input type="submit" name="zeb" value="Zebs" class="btn btn-success btn-sm"/>
