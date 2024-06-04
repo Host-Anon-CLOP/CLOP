723491526fffbf193f1dde77f1dffd78db1cd007
Allen Mitchell <elune1@outlook.com>
2024-06-04 21:22:58 +1200

    adding additional search options

--- clop/search.php
+++ clop/search.php
@@ -6,9 +6,10 @@ echo <<<EOFORM
 <center><form action="search.php" method="post">
 <input name="name" class="form-control" placeholder="User or Nation Name" style="width:300px"/>
 <input type="submit" name="search" value="Search" class="btn btn-success"/>
+<br>
+<br>
 
 <input type="submit" name="allianceless" value="Unallied Players" class="btn btn-success btn-sm"/>
-<br><br>
 <input type="submit" name="prze" value="Prze" class="btn btn-success btn-sm"/>
 <input type="submit" name="zeb" value="Zebs" class="btn btn-success btn-sm"/>
 <input type="submit" name="saddle" value="Saddles" class="btn btn-success btn-sm"/>
